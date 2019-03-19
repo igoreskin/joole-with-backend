7783531302d13ccc6155047bc9184457cd79043a
Igor Eskin <eskinhd@gmail.com>
2019-03-19 18:12:35 -0400

    Login page styling completed

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -11,18 +11,28 @@ class LoginPage extends Component {
             <div className={styles.container}>
                 <div className={styles.signup}>Sign up</div>
 
-                <div className={styles.logo}>joole</div>
+                {/* <div className={styles.logo}>joole</div> */}
 
-                <div className={cx(globalStyles.container)}>
-                    <div className={cx(globalStyles.row)}>
-                        <div className={cx(globalStyles.col, globalStyles['col-sm-2'])}>in column</div>
-                        <div className={cx(globalStyles.col, globalStyles['col-sm-6'])}>in column</div>
-                        <div className={cx(globalStyles.col, globalStyles['col-sm-2'])}>in column</div>
-                        <div className={cx(globalStyles.col, globalStyles['col-sm-2'])}>in column</div>
+                <div className={styles.logoContainer}>
+                    <div className={cx(globalStyles['container-fluid'])}>
+                        <div className={cx(globalStyles.row, styles.logo)}>
+                            <div className={cx(globalStyles.col, globalStyles['col-sm-2'], styles.column)}>j</div>
+                            <div className={cx(globalStyles.col, globalStyles['col-sm-6'], styles.column)}>
+                            <img src='../../third-attempt-logo.svg' className={styles.svgLogo} />
+                            {/* <span><svg width="52" height="52" className={styles.circle}>
+                                <circle cx="26" cy="26" r="25" fill="#1F4F7B" opacity="0.4" />
+                            </svg></span>
+                            <span><svg width="52" height="52" className={styles.circle}>
+                                <circle cx="26" cy="26" r="25" fill="#1F4F7B" opacity="0.4" />
+                            </svg></span> */}
+                            </div>
+                            <div className={cx(globalStyles.col, globalStyles['col-sm-2'], styles.column)}>l</div>
+                            <div className={cx(globalStyles.col, globalStyles['col-sm-2'], styles.column)}>e</div>
+                        </div>
                     </div>
                 </div>
 
-                <p>Building Product Selection Platform</p>
+                <p className={styles.building}>Building Product Selection Platform</p>
 
                 <form className={styles.inputForm}>
                     {/* <span class="input-group-addon"><i class="fas fa-user"></i></span> */}
@@ -38,9 +48,9 @@ class LoginPage extends Component {
 
                 </form>
 
-                <svg width="100" height="100">
+                {/* <svg width="100" height="100">
                     <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" />
-                </svg>
+                </svg> */}
             </div>
         )
     }
