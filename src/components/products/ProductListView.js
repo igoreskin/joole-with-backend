30c7146176d8812349a819c5a8bb920b24e80d70
Igor Eskin <eskinhd@gmail.com>
2019-03-26 16:35:45 -0400

    props.product assigned to a variable in ProductListView

--- src/components/products/ProductListView.js
+++ src/components/products/ProductListView.js
@@ -5,38 +5,39 @@ import styles from './ProductListView.module.css';
 
 const ProductListView = (props) => {
     console.log(props.product)
+    const data = props.product;
     return(
         <div className={styles.listView}>
 
             <p className={styles.verified}>Verified 08/21/2016</p>
 
             <div>
-                <img src={props.product.image} className={styles.fan} />
+                <img src={data.image} className={styles.fan} />
             </div>
 
             <div className={styles.largeDataDiv}>
-                <div className={styles.largeDataLine}>{props.product.name}</div>
-                <div className={styles.largeDataLine}>{props.product.series}</div>
-                <div className={styles.largeDataLine}>{props.product.code}</div>
+                <div className={styles.largeDataLine}>{data.name}</div>
+                <div className={styles.largeDataLine}>{data.series}</div>
+                <div className={styles.largeDataLine}>{data.code}</div>
             </div>
 
             <div className={styles.strip}>
-                <div className={styles.listViewData}>{props.product.technical_data.airflow} CFM</div>
-                <div className={styles.listViewData}>{props.product.technical_data.power} W at max speed</div>
-                <div className={styles.listViewData}>{props.product.technical_data.noise} dBA at max speed</div>
-                <div className={styles.listViewData}>{props.product.technical_data.size} fan sweep diameter</div>
+                <div className={styles.listViewData}>{data.technical_data.airflow} CFM</div>
+                <div className={styles.listViewData}>{data.technical_data.power} W at max speed</div>
+                <div className={styles.listViewData}>{data.technical_data.noise} dBA at max speed</div>
+                <div className={styles.listViewData}>{data.technical_data.size} fan sweep diameter</div>
             </div>
 
             <div className={styles.pastSpecs}>
                 <div className={styles.pastSpecsLine}>Past specifications:</div>
-                <div className={styles.pastSpecsLine}>{props.product.past}</div>
+                <div className={styles.pastSpecsLine}>{data.past}</div>
             </div>
 
 
             <div className={styles.compareAdd}>
                 <div className={cx(globalStyles["custom-control"], globalStyles["custom-checkbox"])}>
-                    <input type="checkbox" className={cx(globalStyles["custom-control-input"], styles.customInput)} id={props.product.code} />
-                    <label className={cx(globalStyles["custom-control-label"], styles.customLabel)} for={props.product.code}>Compare</label>
+                    <input type="checkbox" className={cx(globalStyles["custom-control-input"], styles.customInput)} id={data.code} />
+                    <label className={cx(globalStyles["custom-control-label"], styles.customLabel)} for={data.code}>Compare</label>
                 </div>
 
                 <div className={styles.addButton}>
