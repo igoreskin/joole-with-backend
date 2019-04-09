070e2e9f60b08baff564b73c567d9a954640729f
Igor Eskin <eskinhd@gmail.com>
2019-04-09 15:55:46 -0400

    Products fetched from the database and displayed

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,15 +1,16 @@
 import axios from 'axios';
+// import fetch from 'isomorphic-fetch';
 
 export function loadProducts() {
     return(dispatch) => {
-        dispatch({type: "LOADING_USERS"});
-        return axios.get('products.json')
-        .then(response => {const products = response.data;
+        dispatch({type: "LOADING_PRODUCTS"});
+        return axios.get('http://localhost:3000/api/products')
+        .then(response => {const products = response.data.products;
+            console.log(products)
             if(!localStorage.getItem('products')) {
                 localStorage.setItem('products', JSON.stringify(products))
             }
             return (dispatch({type: 'LOAD_PRODUCTS', payload: products}, 
-            // console.log(users)
             ))
         })
         .catch(error => console.log(error))
