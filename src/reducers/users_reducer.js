14cc659ea37d5c025b9031c0666cfcfce81c0029
Igor Eskin <eskinhd@gmail.com>
2019-05-08 14:46:14 -0400

    Typo corrected in users_reducer

--- src/reducers/users_reducer.js
+++ src/reducers/users_reducer.js
@@ -1,7 +1,7 @@
 function usersReducer(state = {loading: false, users: []}, action) {
   switch(action.type) {
     case 'LOADING_USERS':
-      return {...state, lading: true}
+      return {...state, loading: true}
     case 'LOAD_REGISTERED_USERS': 
       console.log(action.payload);
       return {loading: false, users: action.payload}
